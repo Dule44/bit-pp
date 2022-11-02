65912ea5050318b7c4de80b5e6fc1c4dc2b097cf
Dusan <aleks29444@gmail.com>
2022-11-02 16:07:56 +0100

    commit

--- functions-intro/ehercise_function5.js
+++ functions-intro/ehercise_function5.js
@@ -76,6 +76,97 @@ studentGrades(
 
 //4. Sort a previously defined array. Place its sorted values into a new array whose values are equivalent to the first array's values multiplied by 2.
 
+function definedArray(array) {
+  result = [];
+  var minElement = array[0];
+  var position = 0;
+  for (var i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] !== undefined) {
+        if (array[j] < minElement) {
+          minElement = array[j];
+          position = j;
+        }
+      }
+    }
+    result[i] = minElement * 2;
+    array[position] = undefined;
+    minElement = Infinity;
+  }
+  return result;
+}
+console.log(definedArray([13, 11, 15, 5, 6, 1, 8, 12]));
+
+// 5.Sort a previously defined array in a descending order and display it in the console.
+// Input:  [ 13, 11, 15, 5, 6, 1, 8, 12 ]
+// Output: [ 15, 13, 12, 11, 8, 6, 5, 1 ]
+
+function descendingArray(array) {
+  result = [];
+  var maxElement = array[0];
+  var position = 0;
+  for (var i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] !== undefined) {
+        if (array[j] > maxElement) {
+          maxElement = array[j];
+          position = j;
+        }
+      }
+    }
+    result[i] = maxElement;
+    array[position] = undefined;
+    maxElement = -Infinity;
+  }
+  return result;
+}
+console.log(descendingArray([13, 11, 15, 5, 6, 1, 8, 12]));
+
+// 6.Write a program that uses a loop to add all the even numbers from 1 to 1000 and subtracts all the odd numbers 1 to 500 from the calculated sum. The result should then be multiplied by 12.5 and displayed in console.
+// Output: 2350000
+
+function loopAdd() {
+  var sum = 0;
+  for (var i = 1; i <= 1000; i++) {
+    if (i % 2 === 0) {
+      sum += i;
+    } else if (i % 2 !== 0 && i <= 500) sum -= i;
+  }
+  sum *= 12.5;
+  return sum;
+}
+console.log(loopAdd());
+
+// 7.Define a 10 element array. Take the first two letters from every string (that has at least 2 letters) in the array and create a new string from them. Print it out in the console.
+// Input: [ "M", "Anne", 12, "Steve", "Joe", "John", "David", "Mark", true, "A" ]
+// Output: AnStJoJoDaMa
+
+function firstTwoLetters(array) {
+  var result = "";
+  var string = "";
+  for (var i = 0; i < array.length; i++) {
+    if (typeof array[i] === "string" && array[i].length >= 2) {
+      string = array[i];
+      result += string[0] + string[1];
+    }
+  }
+  return result;
+}
+console.log(
+  firstTwoLetters([
+    "M",
+    "Anne",
+    12,
+    "Steve",
+    "Joe",
+    "John",
+    "David",
+    "Mark",
+    true,
+    "A",
+  ])
+);
+
 //8.Write a program that takes a string and prints its characters out in reversed order in the console.
 
 function reverseString(str) {
@@ -86,6 +177,26 @@ function reverseString(str) {
   return newString;
 }
 console.log(reverseString("Belgrade Institute of Technology"));
+
+// 9.Write a program that displays all the combinations of two numbers between 1 and 7. Don't display two of the same numbers at the same time. Display the number of possible combinations, as well. (E.g. (1.2),(2,1) is allowed, but not (1,1), (2,2)...).
+
+function possibleCombinations() {
+  var count = 0;
+  var result = "";
+  var message;
+  for (var i = 1; i <= 7; i++) {
+    for (j = 1; j <= 7; j++) {
+      if (i !== j) {
+        result += "(" + i + "," + j + ")\n";
+        count++;
+      }
+    }
+  }
+  message = result + count;
+  return message;
+}
+console.log(possibleCombinations());
+
 
 
 //10.Write a program that checks if the entered number is a prime number (i.e. divisible only by 1 and by itself).
@@ -103,16 +214,23 @@ console.log(isPrime(15));
 
 //11.Check if a given string is a palindrome (spaces are ignored).
 
-function palindrome(str) {
-  var re = /[^A-Za-z0-9]/g;
-  str = str.toLowerCase().replace(re, "");
-  var len = str.length;
-  for (var i = 0; i < len / 2; i++) {
-    if (str[i] !== str[len - 1 - i]) {
-      return false;
+function palindrome(string) {
+  var reverse = "";
+  var stringMod = "";
+  for (var i = string.length - 1; i >= 0; i--) {
+    if (string[i] !== " ") {
+      reverse += string[i];
     }
   }
-  return true;
+  for (var j = 0; j < string.length; j++) {
+    if (string[j] !== " ") {
+      stringMod += string[j];
+    }
+  }
+  if (stringMod === reverse) {
+    return true;
+  }
+  return false;
 }
 console.log(palindrome("eye"));
 console.log(palindrome("Geek "));
