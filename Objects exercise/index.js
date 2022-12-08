321ecae31ebf53436cc6cbb49ec7a0a40570fd09
Dusan <aleks29444@gmail.com>
2022-12-08 16:00:56 +0100

    commit

---
+++ Objects exercise/index.js
@@ -0,0 +1,34 @@
+//1. Create an object that represents your favourite coffee. Please include coffee name,
+//strength, flavour, milk, sugar, … everything you like!
+
+var coffee = {
+  name: "Nes",
+  strength: "medium",
+  flavour: "vanilla",
+  milk: "yes",
+  sugar: "no",
+};
+
+console.log(coffee);
+
+//2. Create an object that represents your favourite movie. Please include title, actors,
+//director, genre, popularity.
+var favMovie = {
+  title: "Indiana Jones and the Last Crusade",
+  actors: ["Harrison Ford", "Sean Connery", "Denholm Elliott"],
+  director: "Steven Spielberg",
+  genre: ["Adventure", "Action"],
+  popularity: {
+    imdb: 8.2,
+    rotten: "90%",
+  },
+};
+
+console.log(favMovie);
+
+// 3. Write a function that creates an object that represents a project. Each project is
+// described by: description, programming language, git repository, boolean status that
+// says if the project is in development or not. Add a method that prints out the project&#39;s
+// repository, a method that checks if the project is written in JavaScript as well as a
+// method that checks if the project is in development or not.
+
